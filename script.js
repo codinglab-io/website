a9563a976eec6fff1c5bcde09e4d2b770e80ac9a
neolectron <neolectron@codinglab.io>
2021-12-08 14:34:03 +0100

    style(prettier): add singlequote option

--- script.js
+++ script.js
@@ -1,78 +1,78 @@
 // 🥚 EasterEgg
 function _0x1468() {
   const _0x294f23 = [
-    "\x20Easter",
-    "#00ff00",
-    "keyCode",
-    "as\x20only",
-    "\x20of\x20cul",
-    "l\x20work\x20",
-    "4mNvmQI",
-    "3775068rZbpUN",
-    "\x20be\x20wor",
-    "e\x20same\x20",
-    "tListen",
-    "183496kAJehm",
-    "ayer",
-    "indefin",
-    "12tBdltx",
-    "For\x20now",
-    "href",
-    "ture,\x20w",
-    "7913950GqAjJZ",
-    "%cFail\x20",
-    "3214915NlXFaH",
-    "l\x20you\x20f",
-    "Someone",
-    "join\x20us",
-    "\x20good\x20w",
-    "key\x20wil",
-    "addEven",
-    "color:\x20",
-    "4w9WgXc",
-    "r\x20you\x20c",
-    "But\x20do\x20",
-    "that\x20!",
-    "9ihBRqY",
-    "126CauHrw",
-    "1223754rjfgvb",
-    "re\x20a\x20pl",
-    "locatio",
-    "Hum...\x20",
-    "So\x20clos",
-    "Good\x20ca",
-    "1iPKzPb",
-    "https:/",
-    "om/watc",
-    "lude\x20?",
-    "all",
-    ",\x20you\x20a",
-    "/www.yo",
-    "keydown",
-    "\x20hit\x20th",
-    "0px;",
-    "log",
-    "lly\x20thi",
-    "35398451fIdWxi",
-    "...it\x20w",
-    "utube.c",
-    "thy\x20to\x20",
-    "%c🥚\x20Wil",
-    "ntinue\x20",
-    "an\x20conc",
-    "re\x20in\x20a",
-    ";\x20font-",
-    "nk\x20that",
-    "so\x20you\x27",
-    "\x20closer",
-    "length",
-    "itely\x20?",
-    "ind\x20our",
-    "e\x20!\x20😱",
-    "232078PZNnHD",
-    "size:\x202",
-    "#ff0000",
-    "h?v=dQw",
+    '\x20Easter',
+    '#00ff00',
+    'keyCode',
+    'as\x20only',
+    '\x20of\x20cul',
+    'l\x20work\x20',
+    '4mNvmQI',
+    '3775068rZbpUN',
+    '\x20be\x20wor',
+    'e\x20same\x20',
+    'tListen',
+    '183496kAJehm',
+    'ayer',
+    'indefin',
+    '12tBdltx',
+    'For\x20now',
+    'href',
+    'ture,\x20w',
+    '7913950GqAjJZ',
+    '%cFail\x20',
+    '3214915NlXFaH',
+    'l\x20you\x20f',
+    'Someone',
+    'join\x20us',
+    '\x20good\x20w',
+    'key\x20wil',
+    'addEven',
+    'color:\x20',
+    '4w9WgXc',
+    'r\x20you\x20c',
+    'But\x20do\x20',
+    'that\x20!',
+    '9ihBRqY',
+    '126CauHrw',
+    '1223754rjfgvb',
+    're\x20a\x20pl',
+    'locatio',
+    'Hum...\x20',
+    'So\x20clos',
+    'Good\x20ca',
+    '1iPKzPb',
+    'https:/',
+    'om/watc',
+    'lude\x20?',
+    'all',
+    ',\x20you\x20a',
+    '/www.yo',
+    'keydown',
+    '\x20hit\x20th',
+    '0px;',
+    'log',
+    'lly\x20thi',
+    '35398451fIdWxi',
+    '...it\x20w',
+    'utube.c',
+    'thy\x20to\x20',
+    '%c🥚\x20Wil',
+    'ntinue\x20',
+    'an\x20conc',
+    're\x20in\x20a',
+    ';\x20font-',
+    'nk\x20that',
+    'so\x20you\x27',
+    '\x20closer',
+    'length',
+    'itely\x20?',
+    'ind\x20our',
+    'e\x20!\x20😱',
+    '232078PZNnHD',
+    'size:\x202',
+    '#ff0000',
+    'h?v=dQw',
   ];
   _0x1468 = function () {
     return _0x294f23;
@@ -94,9 +94,9 @@ const _0x24fcab = _0x4764;
         (parseInt(_0x3c6db6(0xb2)) / 0x9) * (parseInt(_0x3c6db6(0xec)) / 0xa) +
         (parseInt(_0x3c6db6(0xc6)) / 0xb) * (parseInt(_0x3c6db6(0xe8)) / 0xc);
       if (_0xbafd17 === _0x2690d9) break;
-      else _0x2757e7["push"](_0x2757e7["shift"]());
+      else _0x2757e7['push'](_0x2757e7['shift']());
     } catch (_0x5e3d8e) {
-      _0x2757e7["push"](_0x2757e7["shift"]());
+      _0x2757e7['push'](_0x2757e7['shift']());
     }
   }
 })(_0x1468, 0xc8e8b),
@@ -105,17 +105,17 @@ const _0x24fcab = _0x4764;
       _0x24fcab(0xa7) +
       _0x24fcab(0xd4) +
       _0x24fcab(0xda) +
-      "Egg?",
-    "color:\x20" +
+      'Egg?',
+    'color:\x20' +
       _0x24fcab(0xd8) +
       _0x24fcab(0xce) +
       _0x24fcab(0xd7) +
       _0x24fcab(0xc3)
   );
 const _0x4efa6d = [
-    _0x24fcab(0xb9) + "tch,\x20co" + _0x24fcab(0xcb) + "😉",
+    _0x24fcab(0xb9) + 'tch,\x20co' + _0x24fcab(0xcb) + '😉',
     _0x24fcab(0xb0) +
-      "you\x20rea" +
+      'you\x20rea' +
       _0x24fcab(0xc5) +
       _0x24fcab(0xcf) +
       _0x24fcab(0xc2) +
@@ -128,10 +128,10 @@ const _0x4efa6d = [
       _0x24fcab(0xbf) +
       _0x24fcab(0xcd) +
       _0x24fcab(0xaa) +
-      "ay...",
+      'ay...',
     _0x24fcab(0xc7) +
       _0x24fcab(0xdd) +
-      "\x20luck\x20o" +
+      '\x20luck\x20o' +
       _0x24fcab(0xaf) +
       _0x24fcab(0xcc) +
       _0x24fcab(0xbd),
@@ -139,15 +139,15 @@ const _0x4efa6d = [
     _0x24fcab(0xa8) +
       _0x24fcab(0xde) +
       _0x24fcab(0xeb) +
-      "e\x20like\x20" +
+      'e\x20like\x20' +
       _0x24fcab(0xb1),
-    "You\x20may" +
+    'You\x20may' +
       _0x24fcab(0xe2) +
       _0x24fcab(0xc9) +
       _0x24fcab(0xa9) +
-      "\x20after\x20" +
+      '\x20after\x20' +
       _0x24fcab(0xbe),
-    "Getting" + _0x24fcab(0xd1) + "\x20!",
+    'Getting' + _0x24fcab(0xd1) + '\x20!',
     _0x24fcab(0xb8) + _0x24fcab(0xd5),
   ],
   _0x2db609 = [0x26, 0x26, 0x28, 0x28, 0x25, 0x27, 0x25, 0x27, 0x42, 0x41];
@@ -166,42 +166,42 @@ let _0x108ab1 = 0x0;
 const _0x1a7c90 = function (_0x18a9d1) {
   const _0x4c684f = _0x24fcab;
   if (
-    _0x2db609["indexOf"](_0x18a9d1[_0x4c684f(0xdc)]) < 0x0 ||
-    _0x18a9d1["keyCode"] !== _0x2db609[_0x108ab1]
+    _0x2db609['indexOf'](_0x18a9d1[_0x4c684f(0xdc)]) < 0x0 ||
+    _0x18a9d1['keyCode'] !== _0x2db609[_0x108ab1]
   ) {
     (_0x108ab1 = 0x0),
       console[_0x4c684f(0xc4)](
-        _0x4c684f(0xa5) + "😞",
+        _0x4c684f(0xa5) + '😞',
         _0x4c684f(0xad) +
           _0x4c684f(0xd8) +
-          ";\x20font-" +
-          "size:\x202" +
+          ';\x20font-' +
+          'size:\x202' +
           _0x4c684f(0xc3)
       );
     return;
   }
   _0x108ab1 !== _0x4efa6d[_0x4c684f(0xd2)] &&
     console[_0x4c684f(0xc4)](
-      "%c" + _0x4efa6d[_0x108ab1],
+      '%c' + _0x4efa6d[_0x108ab1],
       _0x4c684f(0xad) +
         _0x4c684f(0xdb) +
         _0x4c684f(0xce) +
-        "size:\x201" +
+        'size:\x201' +
         _0x4c684f(0xc3)
     ),
     _0x108ab1++,
-    _0x2db609["length"] === _0x108ab1 &&
+    _0x2db609['length'] === _0x108ab1 &&
       ((_0x108ab1 = 0x0),
-      (window[_0x4c684f(0xb6) + "n"][_0x4c684f(0xea)] =
+      (window[_0x4c684f(0xb6) + 'n'][_0x4c684f(0xea)] =
         _0x4c684f(0xbb) +
         _0x4c684f(0xc0) +
         _0x4c684f(0xc8) +
         _0x4c684f(0xbc) +
         _0x4c684f(0xd9) +
         _0x4c684f(0xae) +
-        "Q"));
+        'Q'));
 };
-document[_0x24fcab(0xac) + _0x24fcab(0xe4) + "er"](
+document[_0x24fcab(0xac) + _0x24fcab(0xe4) + 'er'](
   _0x24fcab(0xc1),
   _0x1a7c90,
   ![]
